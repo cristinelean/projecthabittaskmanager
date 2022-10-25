1882c6aa8598b4a73db2c2533d00d795bc515106
Lean Cristine Bugay <leancristine.bugay.cics@ust.edu.ph>
2022-10-25 13:17:00 +0800

    New Update

---
+++ app/src/main/java/com/bugay/ui/PlannerActivity.java
@@ -0,0 +1,14 @@
+package com.bugay.ui;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlannerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_planner);
+    }
+}
