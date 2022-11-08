ca6f8499ca6f69779abed86cc7ece2ef9b252d88
Lean Cristine Bugay <leancristine.bugay.cics@ust.edu.ph>
2022-11-08 14:52:23 +0800

    v11

--- app/src/main/java/com/bugay/ui/CreateTask.java
+++ app/src/main/java/com/bugay/ui/CreateTask.java
@@ -10,26 +10,10 @@ import android.widget.TextView;
 
 public class CreateTask extends AppCompatActivity {
 
-    private Button back;
-    private TextView mclndr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_task);
-        back = findViewById(R.id.btnBack);
-        mclndr = findViewById(R.id.date);
-
-        Intent incomingIntent = getIntent();
-        String date = incomingIntent.getStringExtra("date");
-        mclndr.setText(date);
-
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(CreateTask.this, TaskActivity.class);
-                startActivity(intent);
-            }
-        });
 
     }
 }
--- app/src/main/java/com/bugay/ui/TaskActivity.java
+++ app/src/main/java/com/bugay/ui/TaskActivity.java
@@ -10,35 +10,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class TaskActivity extends AppCompatActivity {
 
-    private Button back, addtask;
-    private TextView mclndr;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
-        mclndr = findViewById(R.id.date);
-        back = findViewById(R.id.btnBack);
-        addtask = findViewById(R.id.btnAddTask);
-
-        Intent incomingIntent = getIntent();
-        String date = incomingIntent.getStringExtra("date");
-        mclndr.setText(date);
-
-        addtask.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(TaskActivity.this, CreateTask.class);
-                intent.putExtra("date", date);
-                startActivity(intent);
-            }
-        });
-
-        back.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(TaskActivity.this, PlannerActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 }
