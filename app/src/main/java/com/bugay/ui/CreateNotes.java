a2e78b0b96e13ebe26f9f146f4d6b87052ef5360
kan-laon <tomasemmanuel.albero.cics@ust.edu.ph>
2022-11-09 23:22:01 +0800

    shared done 2/5

--- app/src/main/java/com/bugay/ui/CreateNotes.java
+++ app/src/main/java/com/bugay/ui/CreateNotes.java
@@ -2,6 +2,7 @@ package com.bugay.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -11,11 +12,16 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class CreateNotes extends AppCompatActivity {
 
     TextView Notes;
     EditText eTitle, eDesc;
-    Button Bshared, Bapply, Bsavis, Bsavees, Bsaveic, Bsaveec;
+    Button Bshared, Bapply, Bsaveis, Bsavees, Bsaveic, Bsaveec;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,11 +32,11 @@ public class CreateNotes extends AppCompatActivity {
         eDesc = findViewById(R.id.dec);
         Bshared = findViewById(R.id.shButton);
         Bapply = findViewById(R.id.saButton);
-        Bsavis = findViewById(R.id.SaveIs);
+        Bsaveis = findViewById(R.id.SaveIs);
         Bsavees = findViewById(R.id.SaveEs);
         Bsaveic = findViewById(R.id.SaveIC);
         Bsaveec = findViewById(R.id.SaveEC);
-        
+
         Bapply.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -54,6 +60,31 @@ public class CreateNotes extends AppCompatActivity {
                 editor.apply();
 
                 Toast.makeText(CreateNotes.this, "Data Saved", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Bsaveis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String a = eTitle.getText().toString();
+                String b = eDesc.getText().toString();
+                String data = a+" "+b;
+
+                FileOutputStream fos = null;
+                try {
+                    fos = openFileOutput("Notes.txt", Context.MODE_PRIVATE);
+                    fos.write(data.getBytes());
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }finally{
+                    try {
+                        fos.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                Toast.makeText(CreateNotes.this, "Data Saved to Internal Storage", Toast.LENGTH_SHORT).show();
             }
         });
         loadData();
